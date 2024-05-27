13745e3920c451871f11cbeb3acf67e89a110fe4
Emil-Kerimov <159919481+Emil-Kerimov@users.noreply.github.com>
2024-05-27 14:42:34 +0300

    final proj

--- Example/Client/client_exp.bat
+++
@@ -1,2 +0,0 @@
-java -cp .\compute.jar;. -Djava.security.policy=program.policy client.ComputeExp localhost 0.000000001 1
-PAUSE
--- Example/Client/client_pi.bat
+++
@@ -1,2 +0,0 @@
-java -cp .\compute.jar;. -Djava.security.policy=program.policy client.ComputePi localhost 45
-PAUSE
